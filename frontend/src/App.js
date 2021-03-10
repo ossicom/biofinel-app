79994558aab6f6c6599067a38558b5b05c23eec8
Osman Guec <ossicom@hotmail.com>
2021-03-10 19:43:01 +0100

    Bis Add new Route from Product.. erledigt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,38 @@
 import React from 'react';
-import data from './data';
-import Product from './components/Product';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div className='grid-container'>
-      <header class='navigation row'>
-        <div>
-          <a className='brand' href='/'>
-            biofinel
-          </a>
-        </div>
-        <div className='button'>
-          <a href='haselnuesse'>Haselnüsse</a>
-          <a href='oliven'>Oliven</a>
-          <a href='tuerkischer-honig'>Honig</a>
-          <a href='oliven oel'>Oliven Öl</a>
-        </div>
-        <div>
-          <a href='/card'>Warenkorb</a>
-          <a href='/signin'>Anmelden</a>
-        </div>
-      </header>
-      <main>
-        <div>
-          <div className='row center'>
-            {data.products.map((product) => (
-              <Product key={product._id} product={product}></Product>
-            ))}
+    <BrowserRouter>
+      <div className='grid-container'>
+        <header class='navigation row'>
+          <div>
+            <a className='brand' href='/'>
+              biofinel
+            </a>
           </div>
-        </div>
-      </main>
-      <footer className='footer'>
-        <p>All right reserved</p>
-      </footer>
-    </div>
+          <div className='button'>
+            <a href='haselnuesse'>Haselnüsse</a>
+            <a href='oliven'>Oliven</a>
+            <a href='tuerkischer-honig'>Honig</a>
+            <a href='oliven oel'>Oliven Öl</a>
+          </div>
+          <div>
+            <a href='/card'>Warenkorb</a>
+            <a href='/signin'>Anmelden</a>
+          </div>
+        </header>
+        <main>
+          <Route path='/product/:id' component={ProductScreen}></Route>
+          <Route path='/' component={HomeScreen} exact></Route>
+        </main>
+        <footer className='footer'>
+          <p>All right reserved</p>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ProductScreen() {
+  return (
+    <div>
+      product Scrfeenproduct Scrfeenproduct Scrfeenproduct Scrfeenproduct
+      Scrfeen product Scrfeenproduct Scrfeenproduct Scrfeenproduct
+      Scrfeenproduct Scrfeenproduct Scrfeenproduct Scrfeen
+    </div>
+  );
+}
