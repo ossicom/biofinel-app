8b361aee0b6997e6fa6c4c69a060e36852761066
Osman Guec <ossicom@hotmail.com>
2021-03-15 13:15:01 +0100

    redux and thunk implemented

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,17 @@
-import { createStore } from 'redux';
-import data from './data';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { productListReducer } from './reducers/productReducers';
 
 const initialState = {};
-const reducer = (state, action) => {
-  return { products: data.products };
-};
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 
-const store = createStore(reducer, initialState);
+const composeEnhanger = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhanger(applyMiddleware(thunk))
+);
 
 export default store;
