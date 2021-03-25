45ca437707dc2b3d47fa2305e25bd89b1fcbaca8
Osman Guec <ossicom@hotmail.com>
2021-03-25 19:26:25 +0100

    Shipping screen created

---
+++ frontend/src/components/CheckoutSteps.js
@@ -0,0 +1,13 @@
+import { PromiseProvider } from 'mongoose';
+import React from 'react';
+
+export default function CheckoutSteps(props) {
+  return (
+    <div className='row checkout-steps'>
+      <div className={props.step1 ? 'active' : ''}>Einloggen</div>
+      <div className={props.step2 ? 'active' : ''}>Versand Adresse</div>
+      <div className={props.step3 ? 'active' : ''}>Bezahlung</div>
+      <div className={props.step4 ? 'active' : ''}>Bestellung aufgeben</div>
+    </div>
+  );
+}
--- frontend/src/constants/cartConstants.js
+++ frontend/src/constants/cartConstants.js
@@ -1,3 +1,5 @@
 export const CART_ADD_ITEM = 'CART_ADD_ITEM';
 //remove from cart
 export const CART_REMOVE_ITEM = 'CART_REMOVE_ITEM';
+//Versand adresse
+export const CART_SAVE_SHIPPING_ADDRESS = 'CART_SAVE_SHIPPING_ADDRESS';
