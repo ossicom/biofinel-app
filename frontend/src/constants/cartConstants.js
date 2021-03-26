b2e3d92d75de10a5faa90b9c695a9410f55f2273
Osman Guec <ossicom@hotmail.com>
2021-03-26 17:01:47 +0100

    payment screen created

--- frontend/src/constants/cartConstants.js
+++ frontend/src/constants/cartConstants.js
@@ -3,3 +3,5 @@ export const CART_ADD_ITEM = 'CART_ADD_ITEM';
 export const CART_REMOVE_ITEM = 'CART_REMOVE_ITEM';
 //Versand adresse
 export const CART_SAVE_SHIPPING_ADDRESS = 'CART_SAVE_SHIPPING_ADDRESS';
+//Bezahl optionen
+export const CART_SAVE_PAYMENT_METHOD = 'CART_SAVE_PAYMENT_METHOD';
